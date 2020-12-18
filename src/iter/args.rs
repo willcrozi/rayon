087a8193fa49e00f4d3d94d45663bfba89c37758
Will Crozier <willcrozi@gmail.com>
2020-12-18 01:41:42 +0000

    args.rs WIP

--- src/iter/args.rs
+++ src/iter/args.rs
@@ -1,8 +1,6 @@
 // Initial attempt with MapArgs as a struct.
 
-use std::iter::Map;
-use crate::iter::Repeat;
-use std::iter;
+use std::ops::Range;
 
 // We need three distinct objects:
 //
@@ -11,66 +9,232 @@ use std::iter;
 //
 //  * A splittable version
 
-pub(crate) trait Args: IntoIterator + Sized
-    where Self::IntoIter: ExactSizeIterator
+pub(crate) trait Args: Sized + Clone + IntoIterator
 {
     fn len(&self) -> usize;
     fn split_at(self, index: usize) -> (Self, Self);
     fn pop(&mut self) -> Self::Item;
+    fn sub_range(&self, range: Range<usize>) -> Self;
+    // fn map_iter<'f, F, T>(self, map_op: F) -> MapArgsIter<'f, F, T, Self>;
+}
 
-    fn map_item<F, T>(self, map_op: &F, item: T) -> MapArgs<'_, F, T, Self> {
-        MapArgs { item, map_op, args: self }
+#[derive(Clone)]
+enum PartialArgs<'a, A: Args> {
+    Ref(&'a A, Range<usize>),
+    Owned(A),
+}
+
+impl<'a, A> Args for PartialArgs<'a, A>
+    where
+        A: Clone,
+        A: Args + 'a,
+{
+    fn len(&self) -> usize {
+        match self {
+            PartialArgs::Ref(_, range) => { range.len() }
+            PartialArgs::Owned(args) => { args.len() }
+        }
+    }
+
+    fn split_at(self, index: usize) -> (Self, Self) {
+        match self {
+            PartialArgs::Ref(args, range) => {
+                debug_assert!(index <= range.len());
+                let mid = range.start + index;
+                (
+                    PartialArgs::Ref(args, range.start..mid),
+                    PartialArgs::Ref(args, mid..range.end)
+                )
+            }
+            PartialArgs::Owned(args) => {
+                let (left, right) = args.split_at(index);
+                (PartialArgs::Owned(left), PartialArgs::Owned(right))
+            }
+        }
+    }
+
+    fn pop(&mut self) -> Self::Item {
+        match self {
+            PartialArgs::Ref(args, range) => {
+                let mut args = args.sub_range(range.clone());
+                let result = args.pop();
+                *self = PartialArgs::Owned(args);
+                result
+            }
+            PartialArgs::Owned(args) => { args.pop() }
+        }
+    }
+
+    fn sub_range(&self, sub_range: Range<usize>) -> Self {
+        match self {
+            PartialArgs::Ref(args, range) => {
+                let start = range.start + sub_range.start;
+                let end = range.start + sub_range.end;
+
+                PartialArgs::Ref(args, start..end)
+            }
+            PartialArgs::Owned(args) => {
+                PartialArgs::Owned(args.sub_range(sub_range))
+            }
+        }
     }
 }
+
+impl<'a, A: Args> IntoIterator for PartialArgs<'a, A> {
+    type Item = A::Item;
+    type IntoIter = A::IntoIter;
+
+    fn into_iter(self) -> Self::IntoIter {
+        match self {
+            PartialArgs::Ref(args, range) => { args.sub_range(range).into_iter() }
+            PartialArgs::Owned(args) => { args.into_iter() }
+        }
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+impl<'t, T> Args for &'t[T] {
+    fn len(&self) -> usize { <[T]>::len(&self) }
+
+    fn split_at(self, index: usize) -> (Self, Self) {
+        (&self[0..index], &self[index..0])
+    }
+
+    fn pop(&mut self) -> Self::Item {
+        let result = &self[0];
+        *self = &self[1..];
+        result
+    }
+
+    fn sub_range(&self, range: Range<usize>) -> Self {
+        &self[range]
+    }
+}
+
+impl<'t, T> Args for Vec<T>
+where
+    T: Clone,
+{
+    fn len(&self) -> usize { <[T]>::len(&self) }
+
+    fn split_at(mut self, index: usize) -> (Self, Self) {
+        let right = self.split_off(index);
+        (self, right)
+    }
+
+    fn pop(&mut self) -> Self::Item {
+        self.pop().unwrap()
+    }
+
+    fn sub_range(&self, range: Range<usize>) -> Self {
+        self[range].to_vec()
+    }
+}
+
+impl<I> Args for I
+where
+    I: ExactSizeIterator,
+{
+    fn len(&self) -> usize {
+        self.len()
+    }
+
+    fn split_at(self, index: usize) -> (Self, Self) {
+        unimplemented!()
+    }
+
+    fn pop(&mut self) -> Self::Item {
+        self.next().unwrap()
+    }
+
+    fn sub_range(&self, range: Range<usize>) -> Self {
+        unimplemented!()
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+// struct MapArgsIter<'f, F, T, A: Args> {
+//     map_op: &'f F,
+//     item: T,
+//     args: A::IntoIter,
+// }
+//
+// impl<'f, F, T, A, R> Iterator for MapArgsIter<'f, F, T, A>
+// where
+//     F: Fn(T, A::Item) -> R,
+//     A: Args,
+//     T: Clone,
+// {
+//     type Item = R;
+//
+//     fn next(&mut self) -> Option<Self::Item> {
+//         unimplemented!()
+//     }
+// }
+
 
 // TODO Using a struct here might be more restrictive than using a trait. For example it might be
 //      possible to perform splits in a more efficient way when implementing a trait for some
 //      provded type here
-pub(crate) struct MapArgs<'f, F, T, A> {
-    item: T,
-    map_op: &'f F,
-    args: A,
-}
+// pub(crate) struct MapArgs<'f, F, T, A> {
+//     item: T,
+//     map_op: &'f F,
+//     args: A,
+// }
+//
+// impl<'f, F, T, A, R> MapArgs<'f, F, T, A>
+//     where
+//         F: Fn(T, A::Item) -> R,
+//         T: Clone,
+//         A: Args,
+//         A::IntoIter: ExactSizeIterator,
+// {
+//     pub fn split_at(self, index: usize) -> (Self, Self) {
+//         let (l_args, r_args) = self.args.split_at(index);
+//         (
+//             MapArgs { item: self.item.clone(), map_op: self.map_op, args: l_args },
+//             MapArgs { item: self.item, map_op: self.map_op, args: r_args },
+//         )
+//     }
+//
+//     pub fn pop(&mut self) -> R {
+//         (self.map_op)(self.item.clone(), self.args.pop())
+//     }
+// }
+//
+// impl<'f, F, T, A, R> IntoIterator for MapArgs<'f, F, T, A>
+//     where
+//         F: Fn(T, A::Item) -> R,
+//         T: Clone,
+//         A: Args,
+//         A::IntoIter: ExactSizeIterator,
+// {
+//     type Item = R;
+//     type IntoIter = MapArgsIter;
+//
+//     fn into_iter(self) -> Self::IntoIter {
+//
+//     }
+// }
+//
 
-impl<'f, F, T, A, R> MapArgs<'f, F, T, A>
-    where
-        F: Fn(T, A::Item) -> R,
-        T: Clone,
-        A: Args,
-        A::IntoIter: ExactSizeIterator,
-{
-    pub fn split_at(self, index: usize) -> (Self, Self) {
-        let (l_args, r_args) = self.args.split_at(index);
-        (
-            MapArgs { item: self.item.clone(), map_op: self.map_op, args: l_args },
-            MapArgs { item: self.item, map_op: self.map_op, args: r_args },
-        )
-    }
-
-    pub fn pop(&mut self) -> R {
-        (self.map_op)(self.item.clone(), self.args.pop())
-    }
-}
-
-impl<'f, F, T, A, R> IntoIterator for MapArgs<'f, F, T, A>
-    where
-        F: Fn(T, A::Item) -> R,
-        T: Clone,
-        A: Args,
-        A::IntoIter: ExactSizeIterator,
-{
-    type Item = R;
-    type IntoIter = MapArgsIter;
-
-    fn into_iter(self) -> Self::IntoIter {
-
-    }
-}
-
-struct MapArgsIter<'f, 't, F, T, A,> {
-    map_op: &'f F,
-    item: &'t T,
-    args: A::IntoIter,
-}
-
-impl Iterator for MapArgsIter<'f, 't, F, T, A,>
